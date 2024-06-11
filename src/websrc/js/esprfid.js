e67c6830bc988a3a165042ddb54b9a5d8c726967
PVTex <info@pvtex.de>
2024-06-11 21:18:54 +0200

    gitignore: added ESP-Debug

--- src/websrc/js/esprfid.js
+++ src/websrc/js/esprfid.js
@@ -2033,7 +2033,7 @@ function login() {
 
 function getLatestReleaseInfo() {
 
-  $.getJSON("https://api.github.com/repos/esprfid/esp-rfid/releases/latest").done(function(release) {
+  $.getJSON("https://api.github.com/repos/esp-rfid/esp-rfid/releases/latest").done(function(release) {
     var asset = release.assets[0];
     var downloadCount = 0;
     for (var i = 0; i < release.assets.length; i++) {
